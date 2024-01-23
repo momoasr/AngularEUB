46b05c3404af4f770b29354e7f353381e4df47e7
momoasr <monia_akele@yahoo.fr>
2024-01-23 13:37:25 -0500

    create an information and an about components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { InformationComponent } from './information/information.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutUsComponent,
+    InformationComponent
   ],
   imports: [
     BrowserModule,
